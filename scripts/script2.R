fd9eb1af0a9e3dbd28356b49a483d24367f7c974
Mark Andrews <mjandrews.org@gmail.com>
2025-12-09 19:45:36 +0000

    Update script2

--- scripts/script2.R
+++ scripts/script2.R
@@ -17,3 +17,24 @@ train_rmse <- map_dbl(models, ~ get_rmse(.x, data = swiss))
 set.seed(42)
 cv_rmse <- map_dbl(formulas, ~ cv_rmse(.x, data = swiss, k = 10))
 loocv_rmse <- map_dbl(formulas, ~ cv_rmse(.x, data = swiss, k = nrow(swiss)))
+
+# Log likelihood of a model
+logLik(models[["m3"]])
+
+# AIC = -2 log likelihood + 2k
+-2 * logLik(models[["m3"]]) + 2 * 5
+
+# Using the built in in R
+AIC(models[["m3"]])
+
+# AIC for the five models
+swiss_aic <- map_dbl(models, AIC)
+
+# View relationship with loocv
+plot(swiss_aic, loocv_rmse)
+
+# BIC for the five models
+swiss_bic <- map_dbl(models, BIC)
+
+# BIC = -2 log likelihood + k log(n)
+-2 * logLik(models[["m3"]]) + 5 * log(nrow(swiss))
