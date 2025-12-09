31fc68a00db0b47acb2ef1154dde6f6ca824867f
Mark Andrews <mjandrews.org@gmail.com>
2025-12-09 18:04:01 +0000

    Update script2

--- scripts/script2.R
+++ scripts/script2.R
@@ -3,5 +3,13 @@ library(tidyverse)
 
 formulas <- list(
   m1 = Fertility ~ Education,
-  m2 = Fertility ~ Education + Catholic
+  m2 = Fertility ~ Education + Catholic,
+  m3 = Fertility ~ Education + Catholic + Infant.Mortality,
+  m4 = Fertility ~ Education + Catholic + Infant.Mortality + Agriculture,
+  m5 = Fertility ~ Education + Catholic + Infant.Mortality + Agriculture + Examination
 )
+
+# fit lm models according to each formula in formulas
+models <- map(formulas, ~ lm(.x, data = swiss))
+
+train_rmse <- map_dbl(models, ~ get_rmse(.x, data = swiss))
